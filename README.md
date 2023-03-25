aeeedceb49eab35865d14cf4befb5379211823da
Kingshuk Chowdhury <kingshuk.chowdhury@modirum.com>
2023-03-25 12:19:51 +0200

    PNG to text

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # png_to_text
 A small Python script that converts PNG to text
+
+## If you don't have tessaract-ocr and libtesseract-dev installed, please install by running below command."
+```bash
+sudo apt-get install tesseract-ocr && sudo apt-get install libtesseract-dev
+```
+
+## Clone the repository and run using `python3` and when it ask for input provide the full path to the PNG format image file that contains text that you want to convert.
+
+```
+git clone https://github.com/kinghub007/png_to_text.git
+cd png_to_text
+python3 image_to_text.py
+```
+
---
+++ image_to_text.py
@@ -0,0 +1,30 @@
+print("If you don't have tessaract-ocr and libtesseract-dev installed, please install by running below command.")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+print("sudo apt-get install tesseract-ocr && sudo apt-get install libtesseract-dev")
+print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+from PIL import Image
+from pytesseract import pytesseract
+
+# Defining paths to tesseract
+# and the image we would be using
+
+path_to_tesseract = r"/usr/bin/tesseract"
+image_path=input("Please enter the full path to the PNG format image: ")
+
+
+#print(image_path)
+
+# Opening the image & storing it in an image object
+img = Image.open(image_path)
+
+# Providing the tesseract executable
+# location to pytesseract library
+pytesseract.tesseract_cmd = path_to_tesseract
+
+# Passing the image object to image_to_string() function
+# This function will extract the text from the image
+text = pytesseract.image_to_string(img)
+
+# Displaying the extracted text
+print("\nThe extracted text is given below:\n")
+print(text[:-1])
